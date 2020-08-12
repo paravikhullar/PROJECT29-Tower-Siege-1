778af09bf3dd01d26d0156018e6a586026935b41
Paravi Khullar <66310260+paravikhullar@users.noreply.github.com>
2020-08-12 11:36:35 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,34 @@
+// class Box extends BaseClass {
+//     constructor(x, y, width, height){
+//       super(x,y,width,height);
+//     //   this.image = loadImage("sprites/wood1.png");
+//     }
+// };
+
+class Box{
+    constructor(x, y, width, height, color) {
+        var options = {
+            'restitution':0.2,
+            'friction':3,
+            'density':1.0
+        }
+        this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        // this.image = loadImage("base.jpg");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("lightblue");
+        rect(0,0,this.width,this.height);
+        // imageMode(CENTER);
+        // image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+  }
